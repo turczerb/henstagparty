befb8ecedb0b325106f8f0c859549aefa5c0b727
turczerb <turczerb@gmail.com>
2025-04-01 18:03:20 +0200

    countdown component works.

--- client/src/components/Admin/DataHandler.jsx
+++ client/src/components/Admin/DataHandler.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 const AdminData = () => {
+  const [navBarData, setNavBarData] = useState([]);
+
   return (
     <div>
       <form>
         <h1>Datas of the website</h1>
-        <input type="text" />
+        <div>
+          <label>Navbar functions</label>
+          <input placeholder="navbar"></input>
+        </div>
       </form>
     </div>
   );
 };
 
 export default AdminData;
+//passworld fUNQqohhUtKAijll
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,17 @@
 //express will be here.
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors()); // CORS engedélyezése
 app.use(express.json()); // JSON body parser middleware
+
+mongoose.connect(
+  "mongodb+srv://turczerb:fUNQqohhUtKAijll@henparty.bhmh9m3.mongodb.net/henparty?retryWrites=true&w=majority&appName=henparty"
+);
 
 // Szerver indítása
 app.listen(port, () => {
