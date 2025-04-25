5bb6581e3d1d2d4778f4ef549161965a7b409cfe
turczerb <turczerb@gmail.com>
2025-04-25 08:01:05 +0200

    working on login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <form>
-        <input placeholder="password" type="password"></input>
+        <input placeholder="username" value={userName}></input>
+        <input placeholder="password" type="password" value={password}></input>
         <button>login</button>
         <Link to="/admin_login">admin login</Link>
       </form>
