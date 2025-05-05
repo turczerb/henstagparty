d8eda479a99cd45d2c1ef66c7a04b611a9be3b2a
turczerb <turczerb@gmail.com>
2025-05-05 20:25:36 +0200

    css on details betteer

--- client/src/components/Details/DetailCard.jsx
+++ client/src/components/Details/DetailCard.jsx
@@ -1,19 +1,48 @@
+import styled from "styled-components"; //css
+const DetailCardContainer = styled.div`
+  color: rgba(176, 146, 89, 1);
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+
+  border-radius: 1rem;
+`;
+
+const InfoRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  flex-wrap: wrap; /* Fontos, hogy engedjük a sorbontást */
+
+  @media (max-width: 768px) {
+    flex-direction: column; /* Mobilon egymás alá */
+  }
+`;
+
+const InfoBox = styled.div`
+  flex: 1;
+  min-width: 280px; /* Garantálja, hogy legalább ennyi helyet foglaljon */
+  padding: 1rem;
+`;
+
 const DetailCard = ({ data }) => {
   if (!data) return <p>Loading...</p>;
 
   return (
-    <div>
+    <DetailCardContainer>
       <>
         {data.map((item, index) => (
-          <div key={index}>
-            <h2>When</h2>
-            <h3>{item.basic_info.time}</h3>
-            <h2>Where</h2>
-            <p>{item.basic_info.infoLocation}</p>
-          </div>
+          <InfoRow key={index}>
+            <InfoBox>
+              <h2>When</h2>
+              <h3>{item.basic_info.time}</h3>
+              <h2>Where</h2>
+              <p>{item.basic_info.infoLocation}</p>
+            </InfoBox>
+          </InfoRow>
         ))}
       </>
-    </div>
+    </DetailCardContainer>
   );
 };
 
--- client/src/components/Details/DetailsContainer.jsx
+++ client/src/components/Details/DetailsContainer.jsx
@@ -1,19 +1,28 @@
 import DetailCard from "./DetailCard";
 import { useData } from "../Context/DataContext";
+import styled from "styled-components"; //css
+
+const FirstContainer = styled.div`
+  background-color: rgba(223, 239, 242, 1);
+  padding: 3rem 1rem;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  color: rgba(176, 146, 89, 1);
+  margin-bottom: 2rem;
+  font-size: 2rem;
+`;
 
 const DetailsContainer = () => {
   const { data } = useData();
 
   return (
-    <div>
-      <div>
-        <h1>The details</h1>
-      </div>
+    <FirstContainer>
+      <Title>The details</Title>
 
-      <div>
-        <DetailCard data={data} />
-      </div>
-    </div>
+      <DetailCard data={data} />
+    </FirstContainer>
   );
 };
 
