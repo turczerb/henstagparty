8310b3e19002a9b8be04427e5cd134e6d4d4d143
turczerb <turczerb@gmail.com>
2025-06-02 11:24:24 +0200

    railway?

--- server/src/index.js
+++ server/src/index.js
@@ -18,12 +18,15 @@ app.use(express.json()); // JSON body parser middleware
 app.use("/auth_user", userRouter);
 app.use("/data", dataRouter);
 
-mongoose.connect(
-  //"mongodb+srv://turczerb:fUNQqohhUtKAijll@henparty.bhmh9m3.mongodb.net/henparty?retryWrites=true&w=majority&appName=henparty"
-  "mongodb+srv://turczerb:fUNQqohhUtKAijll@henparty.bhmh9m3.mongodb.net/henstageapp?retryWrites=true&w=majority&appName=henstageapp"
-);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Szerver indítása
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+//"mongodb+srv://turczerb:fUNQqohhUtKAijll@henparty.bhmh9m3.mongodb.net/henparty?retryWrites=true&w=majority&appName=henparty"
+//ez volt hasznalva  "mongodb+srv://turczerb:fUNQqohhUtKAijll@henparty.bhmh9m3.mongodb.net/henstageapp?retryWrites=true&w=majority&appName=henstageapp"
